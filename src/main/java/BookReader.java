c51e7ff2c922224c2f0a36a15d95f2251432fb4e
Asia <joanna.legiedz@gmail.com>
2018-01-05 10:37:56 +0100

    fist commit on a sample of the book

---
+++ src/main/java/BookReader.java
@@ -0,0 +1,65 @@
+
+import java.io.File;
+import java.util.*;
+
+public class BookReader {
+
+
+    public static void main(String[] args) throws Exception {
+
+        File file = new File("/Users/asia/Desktop/try.txt");
+
+        Scanner scanner = new Scanner(file);
+
+        Map<String, Integer> szekspirWords = new HashMap<>();
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.next();
+            String word = input.toLowerCase();
+            Integer count = szekspirWords.get(word);
+
+            if (count == null) {
+                szekspirWords.put(word, 1);
+            } else {
+                szekspirWords.put(word, count + 1);
+            }
+
+        }
+
+
+        List<Map.Entry<String, Integer>> list1 = sortedList(szekspirWords);
+
+        HashMap<String, Integer> sortedMapByValues = getSortedMap(list1);
+
+        for (Map.Entry mp : sortedMapByValues.entrySet()
+                ) {
+            System.out.println("word: " + mp.getKey() +
+                    " : " + mp.getValue());
+
+        }
+
+
+    }
+
+    private static HashMap<String, Integer> getSortedMap(List<Map.Entry<String, Integer>> list1) {
+        HashMap<String, Integer> sortedMapByValues = new LinkedHashMap<>();
+
+        for (int i = 0; i < list1.size(); i++) {
+            sortedMapByValues.put(list1.get(i).getKey(), list1.get(i).getValue());
+        }
+        return sortedMapByValues;
+    }
+
+    private static List<Map.Entry<String, Integer>> sortedList(Map<String, Integer> szekspirWords) {
+        List<Map.Entry<String, Integer>> list = new LinkedList(szekspirWords.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+        return list;
+    }
+}
+
+
